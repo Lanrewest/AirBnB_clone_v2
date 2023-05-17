be3b182299f3c61c86c41e9100546fd1da73cafa
Ubuntu <ubuntu@crestinc.tech>
2023-05-17 21:49:58 +0000

    readme

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='18.209.225.78', port=5000)
